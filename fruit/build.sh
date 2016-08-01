57cc3eeeab5faffbfa61524cfbd1a062a0d340f0
Ed Herbst <ed.herbst@gmail.com>
2016-08-01 17:02:34 -0400

    initial commit

---
+++ fruit/build.sh
@@ -0,0 +1,9 @@
+cp /home/eherbst/anaconda3/include/mpi.mod .
+which mpif90
+mpif90 -c -fpic -O3 src/fruit.f90
+#mpif90 -c -fpic -O3 src/fruit_mpi.f90
+#mpif90 -shared -o libfruit.so fruit_mpi.o fruit.o
+mpif90 -shared -o libfruit.so fruit.o
+cp libfruit.so $PREFIX/lib
+mkdir -p $PREFIX/include/fruit
+cp *.mod $PREFIX/include/fruit
